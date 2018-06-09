a1661c6ec0490781250a8f0b92a91462673878a3
Michael Daniels <mikeytdan@gmail.com>
2018-06-09 12:35:55 -0600

    Linked the javascript logic up with the html

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,26 +1,35 @@
-
-
-
 window.onload = function (event) {
-    console.log("Loaded!");
-    var coordinate = latLong("84108");
-    if (coordinate == undefined) {
-        console.log("Invalid zip code!");
-        return;
-    }
 
-    console.log(`Coordinate: (${coordinate.lat}, ${coordinate.long})`);
-    getWeather(coordinate.lat, coordinate.long);
-    updatedUVIndex(coordinate.lat, coordinate.long);
-  
+    $("#submit").on("click", function (event) {
+        event.preventDefault();
+
+        $("#trails").empty();
+        var inputText = $("#search-input").val().trim();
+        $("#search-input").val("");
+
+        var coordinate = latLong(inputText);
+        if (coordinate == undefined) {
+            $("#zip-error").text("Invalid zip code!");
+            return;
+        }
+
+        $("#search-input").val("");
+        getTrails(coordinate.lat, coordinate.long);
+        getWeather(coordinate.lat, coordinate.long);
+        updatedUVIndex(coordinate.lat, coordinate.long);
+    })
+
+    $('#search-input').on('input', function (e) {
+        $("#zip-error").text("");
+    });
+
 }
-
 
 function getWeather(lat, long) {
     //current weather by zip code
     var queryWeather = "https://api.openweathermap.org/data/2.5/weather?lat=" +
-    lat + "&lon=" + long + "&units=imperial&APPID=19c1678d8290bb9850e1fdc1681c0dd7";
-    
+        lat + "&lon=" + long + "&units=imperial&APPID=19c1678d8290bb9850e1fdc1681c0dd7";
+
     $.ajax({
         url: queryWeather,
         method: "GET"
@@ -29,17 +38,16 @@ function getWeather(lat, long) {
         var condition = response.weather[0].main;
 
         //add the weather data to the screen
-        $("#weather").text("The current weather is: " + weather);
+        $("#weather").text("The current weather is: " + weather + "F°");
         $("#weather").append("<p>The conditions are: " + condition + " for hiking!</p>");
     });
 
-   
 }
 
 function updatedUVIndex(lat, long) {
     //uv index by zip code
     var queryUVIndex = "https://api.openweathermap.org/data/2.5/uvi?appid=19c1678d8290bb9850e1fdc1681c0dd7&lat=" +
-    lat + "&lon=" + long;
+        lat + "&lon=" + long;
 
     $.ajax({
         url: queryUVIndex,
