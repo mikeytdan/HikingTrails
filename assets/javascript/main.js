a5aba029b95763cc56898a4185cd516b26c8870f
aeffiong <aeffiong9@gmail.com>
2018-06-09 11:23:25 -0600

    cleaning up weather_data code

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -12,7 +12,7 @@ window.onload = function (event) {
     console.log(`Coordinate: (${coordinate.lat}, ${coordinate.long})`);
     getWeather(coordinate.lat, coordinate.long);
     updatedUVIndex(coordinate.lat, coordinate.long);
-    updatedAirPollution(coordinate.lat, coordinate.long);
+  
 }
 
 
@@ -25,12 +25,8 @@ function getWeather(lat, long) {
         url: queryWeather,
         method: "GET"
     }).then(function (response) {
-        console.log(response);
-
         var weather = response.main.temp;
-        console.log(weather);
         var condition = response.weather[0].main;
-        console.log(condition);
 
         //add the weather data to the screen
         $("#weather").text("The current weather is: " + weather);
@@ -42,7 +38,6 @@ function getWeather(lat, long) {
 
 function updatedUVIndex(lat, long) {
     //uv index by zip code
-    // https://iaspub.epa.gov/enviro/efservice/getEnvirofactsUVDAILY/ZIP/20050/JSON
     var queryUVIndex = "https://api.openweathermap.org/data/2.5/uvi?appid=19c1678d8290bb9850e1fdc1681c0dd7&lat=" +
     lat + "&lon=" + long;
 
@@ -50,55 +45,17 @@ function updatedUVIndex(lat, long) {
         url: queryUVIndex,
         method: "GET"
     }).then(function (response) {
-        console.log(response);
-
         var UVIndex = response.value;
-        console.log(UVIndex);
+
         // Adding the data to the screen dynamically
         $("#uv-index").text("The UV Index is: " + UVIndex);
         $("#uv-index").append("<p> Low = 0-2; moderate = 3-5; high = 6-7; very high = 8-10; extreme = 11+</p>");
-        // I can add a link to the UV index link - https://www.epa.gov/sunsafety/uv-index-scale-1
+        //https://www.epa.gov/sunsafety/uv-index-scale-1
     });
 
 }
-// this is only showing for utah zip codes - ahhh
-function updatedAirPollution(lat, long) {
-     //air pollution - air quality index
-     var queryAirPollution = "https://www.airnowapi.org/aq/forecast/latLong/?format=application/json&latitude=" + 
-     lat + "&longitude=" + long + "&API_KEY=FA631898-16EF-4A6B-A039-8839489DC026";
 
 
-    $.ajax({
-        url: queryAirPollution,
-        method: "GET", 
-        headers: {
-            "Access-Control-Allow-Origin": "*"
-        }
-    }).then(function (response) {
-        console.log(response);
-
-        // var airPollution = response[0].AQI;
-        // console.log(airPollution);
-
-        var AQICategory = response[0].Category.Name;
-        console.log(AQICategory);
-
-        var AQIAction = response[0].Discussion;
-        console.log(AQIAction);
-
-        // if AQI = -1, then display the category instead; -1 = no data
-        // if(airPollution === -1) {
-        //     airPollution = "no AQI index today";
-        //     console.log(airPollution);
-        // }
-       
-
-        // adding data to the screen dynamically
-        $("#air-pollution").text("The air quality today is: " + AQICategory);
-        $("#air-pollution").append("<p>" + AQIAction + "</p>");
-
-    })
-}
 
 
 
